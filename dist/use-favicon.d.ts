9a7fd3297de45624fb4765b40321e1befffd263a
Per Enström <enstrom.per@gmail.com>
2023-04-18 20:27:41 +0200

    Fix type error in return
    Returning `as const` will make the function return a tuple of `string` and handlers, not as before an array of either strings or handlers.

--- dist/use-favicon.d.ts
+++ dist/use-favicon.d.ts
@@ -1,10 +1,10 @@
 import * as React from "react";
-declare function useFavicon(): (string | {
+declare function useFavicon(): readonly [string, {
     jsxToFavicon: (SvgEl: React.ReactSVGElement) => void;
     restoreFavicon: () => void;
     drawOnFavicon: (drawCallback: any, { faviconSize, clear, ...props }?: any) => void;
     setFaviconHref: React.Dispatch<React.SetStateAction<string>>;
     setEmojiFavicon: (emoji: any) => void;
-})[];
+}];
 export { useFavicon };
 export default useFavicon;
